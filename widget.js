182285b5bfcdd8476ed43dbb3cf051959db5d3df
karolwiczling <wiczling.karol@tutanota.com>
2018-08-25 12:22:47 +0200

    initial commit

---
+++ widget.js
@@ -0,0 +1,34 @@
+/*
+    show on the bottom-right corner of the page
+    hover - show details
+    clicked - open menu
+*/
+let widgetButton; 
+
+/*
+    allows user to draw on top of the webpage 
+    show simple set of tools (DrawingTools)
+*/
+let widgetEditableMask;
+
+/*
+    show drawings on top of the page 
+    not allow to modify and interact with drawings
+    allow to use website
+*/
+let widgetMask;
+
+/*
+    on/off EditableMask
+    on/off Mask
+    on/off Tools
+    close menu
+*/
+let widgetUserMenu;
+
+/*
+    color picker
+    brush picker
+    move tools window
+*/
+let widgetDrawingTools;
